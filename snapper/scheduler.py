dd74ef88473a05bd2d3990790bbb4a786a10a38a
Oleg Egorov <oleg_egorov@msn.com>
2019-12-13 01:39:32 +0300

    meta: full filename -> basename

--- snapper/scheduler.py
+++ snapper/scheduler.py
@@ -1,17 +1,9 @@
-from functools import partial
 from multiprocessing import Pool
-
-from snapper.worker import copy_template, finish_task, host_worker
 
 
 class Scheduler:
-    def __init__(self, workers, output_paths_format):
+    def __init__(self, workers):
         self.pool = Pool(workers)
-        self._output_paths_format = output_paths_format
 
     async def capture_snaps(self, task):
-        copy_template(task)
-
-        host_worker_call = partial(host_worker, task=task)
-        finish_task_call = partial(finish_task, task=task, output_paths_format=self._output_paths_format)
-        self.pool.map_async(host_worker_call, range(len(task.urls)), callback=finish_task_call)
+        self.pool.map_async(task.process_url, range(len(task.urls)), callback=task.finish_task)
--- snapper/task.py
+++ snapper/task.py
@@ -1,9 +1,19 @@
 import asyncio
+import json
+import logging
 import os
+import shutil
 from pathlib import Path
 from uuid import uuid4
 
+from jinja2 import Environment, PackageLoader
+
 from snapper.scheduler import Scheduler
+from snapper.web_driver import WebDriver
+
+
+env = Environment(autoescape=True,
+                  loader=PackageLoader('snapper', 'templates'))
 
 
 class Task:
@@ -20,19 +30,87 @@ class Task:
         self.user_agent = user_agent
         self.phantomjs_binary = phantomjs_binary
         self.left = len(urls)
+        self.output_paths_format = output_paths_format
+
+        self.copy_template()
 
         if not Task.scheduler:
-            Task.scheduler = Scheduler(workers, output_paths_format)
+            Task.scheduler = Scheduler(workers)
+
+    def copy_template(self):
+        shutil.copytree(Path(__file__).parent / "templates", self.output_path)
+        os.makedirs(self.output_path / "images")
+
+    def fill_template(self):
+        # absolute path to relative path
+        result_with_rel_paths = [
+            (_, os.path.relpath(filename, self.output_path))
+            for _, filename in self.result.items()
+            if filename
+        ]
+
+        sets_of_six = list(zip(*[iter(result_with_rel_paths)]*6))
+        sets_of_six.append(result_with_rel_paths[len(sets_of_six)*6:])
+
+        template = env.get_template('index.html')
+        index_file_path = Path(self.output_path) / "index.html"
+        with open(index_file_path, "w") as index_file:
+            index_file.write(template.render(sets_of_six=sets_of_six))
+
+        self.result.update({"all": str(index_file_path)})
 
     def run(self):
-        if not Path(self.output_path).exists():
-            os.makedirs(self.output_path)
-
         asyncio.create_task(Task.scheduler.capture_snaps(self))
 
     def to_dict(self):
+        result = {
+            url: self.output_paths_format.format(filename)
+            for url, filename in self.result.items()
+        }
+
         return {
             "id": self.id,
             "status": self.status,
-            "result": self.result
+            "result": result
         }
+
+    def process_url(self, url_id):
+        with WebDriver(self.user_agent, self.phantomjs_binary, self.timeout) as web_driver:
+            filename = Path(self.output_path) / "images" / (str(uuid4()) + ".png")
+
+            host = self.urls[url_id]
+            if not host.startswith("https://") and not host.startswith("http://"):
+                host = "http://" + host
+
+            logging.debug("Fetching %s", host)
+            if not WebDriver.host_reachable(host, self.timeout) or \
+                    not web_driver.save_image(host, str(filename)):
+                logging.debug("%s is unreachable or timed out", host)
+
+                try:
+                    error_page_path = Path(__file__).parent / "error_pages" / "unreachable_or_timed_out.png"
+                    shutil.copyfile(error_page_path, filename)
+                except IOError as e:
+                    logging.error("error while coping %s to %s: %s", error_page_path, filename, str(e))
+                    return self.urls[url_id], None
+
+            return self.urls[url_id], str(filename)
+
+    def create_meta_file(self):
+        result_with_basenames = {url: os.path.basename(path) for url, path in self.result.items()}
+
+        with open(Path(self.output_path) / "images" / "meta.txt", "w") as meta_file:
+            json.dump(result_with_basenames, meta_file)
+
+    def finish_task(self, urls_to_filenames):
+        # list of list -> dict
+        self.result.update({
+            url: filename
+            for url, filename in urls_to_filenames
+            if filename
+        })
+
+        self.create_meta_file()
+        self.fill_template()
+
+        self.status = "ready"
